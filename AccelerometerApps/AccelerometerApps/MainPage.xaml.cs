b3d0baed98bc8f6ad80841663fa19385380ef2c0
UserIT <UserIT@ITADMIN>
2023-01-05 11:38:59 +0800

    Add project files.

---
+++ AccelerometerApps/AccelerometerApps/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace AccelerometerApps
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Clicked(object sender, EventArgs e)
+        {
+            if (Accelerometer.IsMonitoring)
+                return;
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+            Accelerometer.Start(SensorSpeed.UI);
+        }
+
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            LabelX.Text = e.Reading.Acceleration.X.ToString();
+            LabelY.Text = e.Reading.Acceleration.Y.ToString();
+            LabelZ.Text = e.Reading.Acceleration.Z.ToString();
+        }
+
+        private void ButtonStop_Clicked(object sender, EventArgs e)
+        {
+            if (!Accelerometer.IsMonitoring)
+                return;
+            Accelerometer.ReadingChanged -= Accelerometer_ReadingChanged;
+            Accelerometer.Stop();
+        }
+    }
+}
